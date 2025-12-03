53548fc10e38ea16b643578a0683f710b1b1f43a
thomasfriquet <thomascbfrt@gmail.com>
2025-12-03 15:56:33 +0100

    add pages

--- code/VehiculeLocation/VehiculeLocation.Frontend/Pages/Add.cshtml.cs
+++ code/VehiculeLocation/VehiculeLocation.Frontend/Pages/Add.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using VehiculeLocation.Frontend.Models;
 
 namespace VehiculeLocation.Frontend.Pages
 {
     public class AddModel : PageModel
     {
+        public Vehicle Vehicle { get; set; } = new Vehicle();
+
         public void OnGet()
         {
         }
