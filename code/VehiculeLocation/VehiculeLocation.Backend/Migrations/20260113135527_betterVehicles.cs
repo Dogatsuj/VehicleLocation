6d06aa22c977b673d3cc884b163cf8aaed7e642b
Dogatsuj <lenourichel.nathan@gmail.com>
2026-01-13 15:07:41 +0100

    privacy

---
+++ code/VehiculeLocation/VehiculeLocation.Backend/Migrations/20260113135527_betterVehicles.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehiculeLocation.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class betterVehicles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "ImagePath" },
+                values: new object[] { "Une voiture qui roule assez bien.", "https://s3-eu-west-1.amazonaws.com/staticeu.izmocars.com/toolkit/commonassets/2024/24renault/24renaultcliotechnohb5rb/24renaultcliotechnohb5rb_animations/colorpix/fr/400x300/renault_24cliotechnohb5rb_orangevalencia_angular-front.webp" });
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Description", "ImagePath" },
+                values: new object[] { "Une voiture qui roule vraiment bien, vous allez voir c'est top !", "https://www.topgear.com/sites/default/files/2024/09/PEUGEOT_3008_EXT_13.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImagePath",
+                value: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/1997_Renault_Twingo_1.15_%281%29.jpg/330px-1997_Renault_Twingo_1.15_%281%29.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Description", "ImagePath" },
+                values: new object[] { "Une voiture qui roule très bien", "https://images.caradisiac.com/logos/5/8/1/4/135814/S8-Citroen-C-40397.jpg" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "ImagePath" },
+                values: new object[] { "Une voiture qui roule", "https://upload.wikimedia.org/wikipedia/commons/6/6d/Dunkerque-1.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Description", "ImagePath" },
+                values: new object[] { "Une voiture qui roule", "https://upload.wikimedia.org/wikipedia/commons/6/6d/Dunkerque-1.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImagePath",
+                value: "https://upload.wikimedia.org/wikipedia/commons/6/6d/Dunkerque-1.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Description", "ImagePath" },
+                values: new object[] { "Une voiture qui roule", "https://upload.wikimedia.org/wikipedia/commons/6/6d/Dunkerque-1.jpg" });
+        }
+    }
+}
--- code/VehiculeLocation/VehiculeLocation.Backend/Seeding/VehicleSeeder.cs
+++ code/VehiculeLocation/VehiculeLocation.Backend/Seeding/VehicleSeeder.cs
@@ -21,8 +21,8 @@ namespace VehiculeLocation.Backend.Data.Seeding
                     DailyRentalPrice = 30.50f,
                     Motorisation = TypeMotorisationEnum.Petrol,
                     IsAutomaticTransmission = false,
-                    Description = "Une voiture qui roule",
-                    ImagePath = "https://upload.wikimedia.org/wikipedia/commons/6/6d/Dunkerque-1.jpg",
+                    Description = "Une voiture qui roule assez bien.",
+                    ImagePath = "https://s3-eu-west-1.amazonaws.com/staticeu.izmocars.com/toolkit/commonassets/2024/24renault/24renaultcliotechnohb5rb/24renaultcliotechnohb5rb_animations/colorpix/fr/400x300/renault_24cliotechnohb5rb_orangevalencia_angular-front.webp",
                     UserId = 1
                 },
                 new Vehicle
@@ -34,8 +34,8 @@ namespace VehiculeLocation.Backend.Data.Seeding
                     DailyRentalPrice = 65.00f,
                     Motorisation = TypeMotorisationEnum.Diesel,
                     IsAutomaticTransmission = true,
-                    Description = "Une voiture qui roule",
-                    ImagePath = "https://upload.wikimedia.org/wikipedia/commons/6/6d/Dunkerque-1.jpg",
+                    Description = "Une voiture qui roule vraiment bien, vous allez voir c'est top !",
+                    ImagePath = "https://www.topgear.com/sites/default/files/2024/09/PEUGEOT_3008_EXT_13.jpg",
                     UserId = 1
                 },
                 new Vehicle
@@ -48,7 +48,7 @@ namespace VehiculeLocation.Backend.Data.Seeding
                     Motorisation = TypeMotorisationEnum.Electric,
                     IsAutomaticTransmission = true,
                     Description = "Une voiture qui roule",
-                    ImagePath = "https://upload.wikimedia.org/wikipedia/commons/6/6d/Dunkerque-1.jpg",
+                    ImagePath = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/1997_Renault_Twingo_1.15_%281%29.jpg/330px-1997_Renault_Twingo_1.15_%281%29.jpg",
                     UserId = 1
                 },
                 new Vehicle
@@ -60,8 +60,8 @@ namespace VehiculeLocation.Backend.Data.Seeding
                     DailyRentalPrice = 35.00f,
                     Motorisation = TypeMotorisationEnum.Petrol,
                     IsAutomaticTransmission = false,
-                    Description = "Une voiture qui roule",
-                    ImagePath = "https://upload.wikimedia.org/wikipedia/commons/6/6d/Dunkerque-1.jpg",
+                    Description = "Une voiture qui roule très bien",
+                    ImagePath = "https://images.caradisiac.com/logos/5/8/1/4/135814/S8-Citroen-C-40397.jpg",
                     UserId = 1
                 }
             };
